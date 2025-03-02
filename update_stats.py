52df46f98744493636847701c4d50b07254886c3
Sriram PR <sriram.periannan@gmail.com>
2025-03-02 17:19:41 +0530

    Updated readme.md

--- update_stats.py
+++ update_stats.py
@@ -24,7 +24,7 @@ if 'USER_NAME' not in os.environ or not os.environ['USER_NAME']:
 USER_NAME = os.environ['USER_NAME']
 HEADERS = {'authorization': 'token ' + os.environ['ACCESS_TOKEN']}
 BIRTH_DATE = datetime.datetime(2005, 2, 14)
-ENABLE_ARCHIVE = os.environ.get('ENABLE_ARCHIVE', 'false').lower() == 'true'
+ENABLE_ARCHIVE = os.environ.get('ENABLE_ARCHIVE', 'true').lower() == 'true'
 QUERY_COUNT = {'user_getter': 0, 'follower_getter': 0, 'graph_repos_stars': 0, 
                'recursive_loc': 0, 'graph_commits': 0, 'loc_query': 0}
 OWNER_ID = None  # Will be set during execution
