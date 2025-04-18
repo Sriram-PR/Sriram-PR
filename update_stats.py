5ef446eb4bfc7d8bddcfb0cadc50d07b601183a1
Sriram PR <160517347+Sriram-PR@users.noreply.github.com>
2025-04-18 22:05:59 +0530

    Update update_stats.py

--- update_stats.py
+++ update_stats.py
@@ -678,11 +678,11 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
         
         find_and_replace(root, 'age_data', age_data)
         justify_format(root, 'commit_data', commit_data, 22)
-        justify_format(root, 'star_data', star_data, 15)
+        justify_format(root, 'star_data', star_data, 14)
         justify_format(root, 'repo_data', repo_data, 7)
         justify_format(root, 'contrib_data', contrib_data)
         justify_format(root, 'follower_data', follower_data, 10)
-        justify_format(root, 'loc_data', loc_data[2], 11)
+        justify_format(root, 'loc_data', loc_data[2], 10)
         justify_format(root, 'loc_add', loc_data[0])
         justify_format(root, 'loc_del', loc_data[1], 7)
         
